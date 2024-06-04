f3ace6cae0280a867b9c92c4723cc2a3277127f1
tdlm987 <trungdlmpk03822@gmail.com>
2024-06-04 19:29:23 +0700

    4 Quái

---
+++ Assets/Trung Work/Scripts/CopController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CopController : MonoBehaviour
+{
+    public Transform posLeft, posRight;
+    public float speed;
+    Vector2 posTarget;
+    private SpriteRenderer spriteCop;
+    public GameObject eyeRay;
+    float direction = 0;
+    public float distance;
+    public Transform playerPos;
+    private Animator anim;
+    public GameObject bulletCop;
+    public Transform posBullet,posBullet1,posBullet2;
+    public float speedBulletAngle;
+    float time;
+    public AudioSource audioShootCop;
+    void Start()
+    {
+        posTarget=posLeft.position;
+        spriteCop=GetComponent<SpriteRenderer>();
+        anim=GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Vector2.Distance(transform.position,posLeft.position) < .1f)
+        {
+            posTarget=posRight.position;
+            spriteCop.flipX= true;
+        }
+        if (Vector2.Distance(transform.position, posRight.position) < .1f)
+        {
+            posTarget = posLeft.position;
+            spriteCop.flipX = false;
+        }
+        if (spriteCop.flipX)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = -1;
+        }
+        transform.position=Vector2.MoveTowards(transform.position, posTarget, speed*Time.deltaTime);
+        if (Mathf.Abs(transform.position.x-playerPos.position.x) < distance)
+        {
+            //Nếu quái ở bên phải người chơi
+            if (transform.position.x > playerPos.position.x)
+            {
+                if (spriteCop.flipX)
+                {
+                    spriteCop.flipX = false;
+                }
+                if (spriteCop.flipX == false)
+                {
+                    speed = 0;
+                    anim.SetBool("OnIdle", true);
+                    anim.speed = 0;
+                    StartShoot();
+                }
+            }
+            //Nếu quái ở bên trái người chơi
+            else
+            {
+                if (spriteCop.flipX==false)
+                {
+                    spriteCop.flipX = true;
+                }
+                if (spriteCop.flipX)
+                {
+                    anim.speed = 0;
+                    speed = 0;
+                    anim.SetBool("OnIdle", true);
+                    StartShoot();
+                }
+            }
+        }
+        else
+        {
+            anim.SetBool("OnIdle", false);
+            speed = 3;
+            anim.speed=1;
+        }
+    }
+    void StartShoot()
+    {
+        time += Time.deltaTime;
+        if (time > 2)
+        {
+            GameObject t= Instantiate(bulletCop, posBullet.position, Quaternion.identity);
+            GameObject t1 = Instantiate(bulletCop, posBullet1.position, posBullet1.rotation);
+            Rigidbody2D r1=t1.GetComponent<Rigidbody2D>();
+            Vector2 directionBullet1 = new Vector2(1, 1).normalized;
+            r1.velocity = directionBullet1*speedBulletAngle;
+            GameObject t2 = Instantiate(bulletCop, posBullet2.position, posBullet2.rotation);
+            Rigidbody2D r2 = t1.GetComponent<Rigidbody2D>();
+            Vector2 directionBullet2 = new Vector2(1, 1).normalized;
+            r1.velocity = directionBullet1 * speedBulletAngle;
+            if (spriteCop.flipX == false)
+            {
+                t.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else
+            {
+                t.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            audioShootCop.Play();
+            time = 0;
+        }
+    }
+}
---
+++ Assets/Trung Work/Scripts/crabMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using UnityEditor;
+using UnityEngine;
+
+public class crabMovement : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform limitLeft,limitRight;
+    [SerializeField] private float speed;
+    private SpriteRenderer crabSprite;
+    Animator animCrab;
+    public GameObject explosionEffect;
+    private Rigidbody2D rb;
+    private float direction;
+    private float healthCrab = 100;
+    private Renderer renderer;
+    private bool isVisible;
+    private void Start()
+    {
+        crabSprite = GetComponent<SpriteRenderer>();
+        animCrab = GetComponent<Animator>();
+        rb= GetComponent<Rigidbody2D>();
+        renderer = GetComponent<Renderer>();
+        direction = 0;
+    }
+    void Update()
+    {
+        // AI di chuyển theo Player
+        if (transform.position.x < player.position.x || transform.position.x < limitLeft.position.x)
+        {
+            crabSprite.flipX = true;
+            direction = 1;
+        }
+        if (transform.position.x > player.position.x || transform.position.x > limitRight.position.x)
+        {
+            crabSprite.flipX = false;
+            direction = -1;
+        }
+        rb.velocity = new Vector2(direction * speed, 0);
+        //Nếu người chơi di chuyển mà AI nằm ngoài tầm nhìn Camera thì sẽ tự động biến mất
+        if (!renderer.isVisible && isVisible )
+        {
+            Destroy(gameObject);
+        }
+        isVisible=renderer.isVisible;
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Nếu nó va chạm với Player thì sẽ tự động dừng lại, sau đó sẽ phát nổ
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Explosion());
+        }
+        if (collision.gameObject.tag == "Bullet")
+        {
+            // Nếu con cua va chạm với đạn của nhân vật thì sẽ bị trừ máu, hết máu thì chết, xóa đạn
+            healthCrab -= 30;
+            if (healthCrab <= 0)
+            {
+                animCrab.SetTrigger("Death2");
+                Destroy(gameObject, 0.6f);
+                Destroy(collision.gameObject, 0.6f);
+            }
+            else
+            {
+                animCrab.SetTrigger("Hurt");
+                Destroy(collision.gameObject, 0.6f);
+            }
+        }
+    }
+    // Hiệu ứng nổ của quái
+    IEnumerator Explosion()
+    {
+        yield return null;
+        Destroy(gameObject);
+        GameObject a=Instantiate(explosionEffect,transform.position,Quaternion.identity);
+        Destroy(a,0.9f);
+    }
+}
